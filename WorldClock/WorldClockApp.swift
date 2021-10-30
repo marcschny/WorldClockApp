c1371168daa911f6a61e1618423047bef5960b1f
Marc Schnydrig <marcschny@MacBook-Pro-von-Marc.local>
2021-10-30 20:57:38 +0200

    empty MVVM structure created

---
+++ WorldClock/WorldClockApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct WorldClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WorldClockView()
+        }
+    }
+}
---
+++ WorldClock/WorldClockView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+
+struct WorldClockView: View{
+    
+    var body: some View{
+        Text("World Clock App")
+            .padding()
+    }
+    
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldClockView()
+    }
+}
