f1eeebc326c7d9c2aea902d29f065bf44d5c7bd4
Marc Schnydrig <marcschny@MacBook-Pro-von-Marc.local>
2021-10-30 20:45:48 +0200

    init

---
+++ WorldClockApp/WorldClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldClockAppApp.swift
+//  WorldClockApp
+//
+//  Created by Marc Schnydrig on 30.10.21.
+//
+
+import SwiftUI
+
+@main
+struct WorldClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
